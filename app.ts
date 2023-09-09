924a825f1c9b3d6b5b2c916273776d856aabe999
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-09-09 12:04:29 -0400

    temp

--- app.ts
+++ app.ts
@@ -1,7 +1,24 @@
+const num1Input  = document.getElementById('input1') as HTMLInputElement;
+const num2Input = document.getElementById('input2')as HTMLInputElement;
+const button = document.getElementById('button') as HTMLButtonElement;
+console.log(num1Input)
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-const result = add(5, 13);
+function printOutput(value): void {
+  console.log(value);
+}
 
-console.log(result);
+// const result  = add(5, 13);
+// let isDone = false
+
+// printOutput(result)
+
+button.addEventListener('click', function () {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  printOutput(result);
+});
