b5d87c8fb9633aa71618ff504778b70f980ee7e3
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-09-09 13:21:33 -0400

    temp

--- wolfgang.ts
+++ wolfgang.ts
@@ -1,4 +1,10 @@
-let isDone = false
+globalThis.l = console.log
 let wolf = function(cbf: Function):number{
-  return 1
+  let temp1 = cbf()
+    l( temp1 * 10)
 }
+
+let kim = function(cbf: Function):number{
+    return 10
+    }
+
